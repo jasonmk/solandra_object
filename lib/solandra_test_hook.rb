77f9c0853ef5ea74ecddbe3d8ef97d6a725bf3ea
Jason M. Kusar <jason@kusar.net>
2011-12-22 09:41:44 +0000

    A hook to allow spec tests to track which classes were used.

---
+++ lib/solandra_test_hook.rb
@@ -0,0 +1,12 @@
+require 'solandra_object'
+module SolandraObject
+  class Base
+    class_attribute :recorded_classes
+    
+    def save_with_record_class(*args)
+      SolandraObject::Base.recorded_classes[self.class] = nil
+      save_without_record_class(*args)
+    end
+    alias_method_chain :save, :record_class
+  end
+end
